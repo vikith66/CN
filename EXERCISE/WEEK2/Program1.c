2019a1a95be7a5a933b5f308d5fcb0354788d640
vikith66 <162144632+vikith66@users.noreply.github.com>
2025-08-04 10:59:06 +0530

    Update Program1.c

--- EXERCISE/WEEK2/Program1.c
+++ EXERCISE/WEEK2/Program1.c
@@ -1,11 +1,9 @@
 // 240970107: Vikith B Kotian
-// Program: Parent sends string to child using pipe
+// C Program: Parent sends string to child using pipe
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <ctype.h>
 #include <sys/wait.h>
 
 int main() {
@@ -26,35 +24,53 @@ int main() {
     }
 
     else if (pid == 0) {
-        close(pipefd[1]); 
+        close(pipefd[1]);
 
         char buffer[100];
         read(pipefd[0], buffer, sizeof(buffer));
 
-        int i, charCount = 0, digitCount = 0;
-        for (i = 0; buffer[i] != '\0'; i++) {
-            if (isalpha(buffer[i]))
+        int charCount = 0, digitCount = 0, i = 0;
+
+        while (buffer[i] != '\0') {
+            if ((buffer[i] >= 'A' && buffer[i] <= 'Z') || 
+                (buffer[i] >= 'a' && buffer[i] <= 'z')) {
                 charCount++;
-            else if (isdigit(buffer[i]))
+            } else if (buffer[i] >= '0' && buffer[i] <= '9') {
                 digitCount++;
+            }
+            i++;
         }
 
         printf("\n[Child] Received string: %s\n", buffer);
         printf("[Child] Number of characters: %d\n", charCount);
         printf("[Child] Number of digits: %d\n", digitCount);
 
-        close(pipefd[0]); // Close read end
+        close(pipefd[0]); 
     }
 
     else {
-        close(pipefd[0]);
+        close(pipefd[0]); 
 
         printf("Enter a string: ");
-        fgets(str, sizeof(str), stdin);
-        str[strcspn(str, "\n")] = '\0'; 
-        
-        write(pipefd[1], str, strlen(str) + 1); 
-        close(pipefd[1]); 
+        scanf(" %[^\n]", str); 
+        int i = 0;
+        while (str[i] != '\0') {
+            if (str[i] == '\n') {
+                str[i] = '\0';
+                break;
+            }
+            i++;
+        }
+
+        int len = 0;
+        while (str[len] != '\0') {
+            len++;
+        }
+
+        write(pipefd[1], str, len + 1); 
+        close(pipefd[1]);              
+
+        wait(NULL); 
     }
 
     return 0;
