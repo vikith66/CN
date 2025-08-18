f032a31c59976ccdc0df37c7ed04676490b42e7a
vikith66 <162144632+vikith66@users.noreply.github.com>
2025-08-18 10:52:17 +0530

    Update server.c

--- EXERCISE/WEEK5/Program1/server.c
+++ EXERCISE/WEEK5/Program1/server.c
@@ -1,1 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE];
+    char client_message[BUFFER_SIZE];
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Waiting for a client to connect...\n");
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                       (socklen_t*)&addrlen)) < 0) {
+        perror("Accept");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Client connected! You can start chatting.\n");
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        memset(client_message, 0, BUFFER_SIZE);
+
+        int valread = read(new_socket, client_message, BUFFER_SIZE);
+        if (valread <= 0) {
+            printf("Client disconnected.\n");
+            break;
+        }
+
+        client_message[valread] = '\0';
+        printf("Client: %s\n", client_message);
+
+        if (strcmp(client_message, "exit") == 0) {
+            printf("Client ended the chat.\n");
+            break;
+        }
+
+        printf("Server: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = '\0';  
+
+        send(new_socket, buffer, strlen(buffer), 0);
+
+        if (strcmp(buffer, "exit") == 0) {
+            printf("Server ended the chat.\n");
+            break;
+        }
+    }
+
+    close(new_socket);
+    close(server_fd);
+
+    return 0;
+}
